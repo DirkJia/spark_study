e6cd2493228618698c5a9cfdd5b1c42588e4ef44
DirkJia <1171475636@qq.com>
2021-03-13 15:32:14 +0800

    fix：test

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,6 +1,87 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
 public class Test {
 
+    @org.junit.Test
     public void test1(){
-        System.out.println("test");
+
+        List<Integer> list = new ArrayList<>();
+        list.remove(1);
+
+        System.out.println(new Test().trap(new int[]{0,1,0,2,1,0,1,3,2,1,2,1}));
     }
+
+    public int trap(int[] height) {
+        int size = height.length;
+        int left =0;
+        int right=size-1;
+        int leftMax =0;
+        int rightMax = 0;
+        int result =0;
+        while(left<right){
+            if(leftMax <= rightMax){
+                leftMax = Math.max(leftMax,height[left]);
+                result += leftMax - height[left++];
+            } else {
+                rightMax = Math.max(rightMax,height[right]);
+                result+=rightMax-height[right--];
+            }
+        }
+        return result;
+    }
+
+    @org.junit.Test
+    public void myAtoi() {
+        String s = "00000-42a1234";
+        char[] chars = s.toCharArray();
+        // 存放合法的数字,栈
+        List<Integer> stack = new ArrayList<>();
+        //符号位
+        boolean isMinus = false;
+        for(int i=0;i<chars.length;i++){
+            char c = chars[i];
+            if(c=='-'){
+                isMinus=true;
+            }
+            if(stack.isEmpty()&&(c=='0'|| c==' ')){
+                continue;
+            }
+
+            if(!Character.isDigit(c)){
+                if(isMinus){
+                    isMinus = false;
+                }
+                break;
+
+            }
+            if(stack.size() >= 10){
+                break;
+            }
+            stack.add((int)(c-'0'));
+        }
+        long result = 0;
+        int count = stack.size()-1;
+        for(int i=0;i<stack.size();i++){
+            result = (long)(Math.pow(10,count))*stack.get(i) + result;
+            count--;
+        }
+        if(isMinus){
+            result = - result;
+        }
+        if(result > Integer.MAX_VALUE){
+            result = Integer.MAX_VALUE;
+        }
+        if(result < Integer.MIN_VALUE){
+            result = Integer.MIN_VALUE;
+        }
+        //return (int) result;
+        Stack<String> a = new Stack<>();
+        a.peek();
+        char c = '4';
+        int ddd = (int)(c-'0');
+    }
+
 }
